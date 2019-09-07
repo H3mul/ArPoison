bed214af1b8528fe296af8c733a7e14e237958d2
Phillip Dykman <phil.d324@gmail.com>
2019-09-06 20:36:02 -0700

    Add frame class heirarchy and factory

--- networktools.py
+++ networktools.py
@@ -1,41 +1,83 @@
 import socket
 import struct
 
-class EthFrame():
-    def __init__(self, socket_buffer=None):
-        self.dst     = socket_buffer[  :6];
-        self.src     = socket_buffer[ 6:12];
-        self.type    = socket_buffer[12:14];
-        self.payload = socket_buffer[14:];
+# An aggregate factory of Frame objects.
+# Sequentially peels layers of frames, stores parsed frame types
+# in separate object members.
+class Frame():
+    def __init__(self, raw_buffer=None):
+        if not raw_buffer:
+            return
 
-        # map protocol constants to their names
-        self.protocol_map = {'0x0806':"ARP"}
+        self.types = []
+        self.addtype("eth", EthFrame(raw_buffer))
 
+        if self.eth.proto_code == "0x0806":
+            self.addtype("arp", ARPFrame(self.eth.payload))
+
+    def pretty(self):
+        pretty_strings = []
+        for frame_type in self.types:
+            if hasattr(self, frame_type):
+                pretty_strings.append(getattr(self, frame_type).pretty())
+        return "\n".join(pretty_strings)
+
+    def addtype(self, type_name, type_frame):
+        setattr(self, type_name, type_frame)
+        self.types.append(type_name)
+        self.type = type_name
+
+
+class BaseFrame():
+    def __init__(self, raw_buffer=None):
+        if raw_buffer:
+            self.parse(raw_buffer) 
+            self.humanReadable()
+    def parse(self, raw_buffer):
+       raise Exception("Must implement parsing") 
+    def humanReadable(self):
+        pass
+    def pretty(self):
+        return vars(self)
+
+
+class EthFrame(BaseFrame):
+    def parse(self, raw_buffer):
+        self.dst_mac_raw = raw_buffer[  :6]
+        self.src_mac_raw = raw_buffer[ 6:12]
+        self.type_raw    = raw_buffer[12:14]
+        self.payload     = raw_buffer[14:]
+
+    def humanReadable(self):
         # human readable IP addresses 
-        self.src_address = "%02X:%02X:%02X:%02X:%02X:%02X" % struct.unpack('BBBBBB', self.src)
-        self.dst_address = "%02X:%02X:%02X:%02X:%02X:%02X" % struct.unpack('BBBBBB', self.dst)
-        self.protocol = "0x%02X%02X" % struct.unpack('BB', self.type)
+        self.src_mac = "%02X:%02X:%02X:%02X:%02X:%02X" % struct.unpack('BBBBBB', self.src_mac_raw)
+        self.dst_mac = "%02X:%02X:%02X:%02X:%02X:%02X" % struct.unpack('BBBBBB', self.dst_mac_raw)
+        self.proto_code = "0x%02X%02X" % struct.unpack('BB', self.type_raw)
 
         # human readable protocol
-        if self.protocol in self.protocol_map:
-            self.protocol = self.protocol_map[self.protocol]
+        protocol_map = {'0x0806':"ARP"}
+        if self.proto_code in protocol_map:
+            self.proto = protocol_map[self.proto_code]
+        else:
+            self.proto = self.proto_code
+
     def pretty(self):
-        return "[ETH] Protocol: %s (%s) -> (%s)" % (self.protocol, self.src_address, self.dst_address)
+        return "[ETH] Protocol: %s (%s) -> (%s)" % (self.proto, self.src_mac, self.dst_mac)
 
-class ARPFrame():
-    def __init__(self, socket_buffer=None):
-        self.eth_frame = EthFrame(socket_buffer)
 
-        self.hwtype         = self.eth_frame.payload[ :2]
-        self.proto_type     = self.eth_frame.payload[2:4]
-        self.hlen           = self.eth_frame.payload[4:5]
-        self.plen           = self.eth_frame.payload[5:6]
-        self.op             = self.eth_frame.payload[6:8]
-        self.sender_hw_addr = self.eth_frame.payload[8:14]
-        self.sender_ip_addr = self.eth_frame.payload[14:18]
-        self.target_hw_addr = self.eth_frame.payload[18:24]
-        self.target_ip_addr = self.eth_frame.payload[24:28]
+class ARPFrame(BaseFrame):
+    def parse(self, raw_buffer):
+        self.hwtype         = raw_buffer[ :2]
+        self.proto_type     = raw_buffer[2:4]
+        self.hlen           = raw_buffer[4:5]
+        self.plen           = raw_buffer[5:6]
+        self.op             = raw_buffer[6:8]
+        self.sender_hw_addr = raw_buffer[8:14]
+        self.sender_ip_addr = raw_buffer[14:18]
+        self.target_hw_addr = raw_buffer[18:24]
+        self.target_ip_addr = raw_buffer[24:28]
 
+    def humanReadable(self):
         self.opcode = int.from_bytes(self.op, byteorder='big', signed=False)
 
         self.sender_mac = "%02X:%02X:%02X:%02X:%02X:%02X" % struct.unpack('BBBBBB', self.sender_hw_addr)
@@ -48,7 +90,7 @@ class ARPFrame():
         self.operation = self.oper_map[self.opcode] if (self.opcode in self.oper_map) else self.opcode
 
     def pretty(self):
-        pretty_string = self.eth_frame.pretty()+"\n[ARP] "
+        pretty_string = "[ARP] "
 
         # ARP request
         if self.opcode == 1:
@@ -57,8 +99,7 @@ class ARPFrame():
         else:
             pretty_string += "%s is at %s -> %s (%s)" % (self.sender_ip, self.sender_mac, self.target_ip, self.target_mac)
         return pretty_string
-
-
+        
 
 device = "eth0"
 packet_count = 10;
@@ -69,11 +110,10 @@ sniffer_socket.bind((device, socket.SOCK_RAW))
 try:
     while packet_count > 0:
         raw_frame = sniffer_socket.recvfrom(65565)[0]
-        eth_frame = EthFrame(raw_frame)
+        frame = Frame(raw_frame)
 
-        if eth_frame.protocol == "ARP":
-            arp_frame = ARPFrame(raw_frame)
-            print(arp_frame.pretty())
+        if frame.type == "arp":
+            print(frame.pretty())
             print()
             --packet_count
 
